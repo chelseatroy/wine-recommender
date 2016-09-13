203d89cfad047db9b0d00c71117739b8571f3461
chelseatroy <chelsea.dommert@gmail.com>
2016-09-13 11:06:55 -0500

    Configure for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wine-recommender.wsgi
--- reviews/forms.py
+++ reviews/forms.py
@@ -4,6 +4,7 @@ from reviews.models import Review
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
+
         fields = ['rating', 'comment']
         widgets = {
             'comment': Textarea(attrs={'cols': 40, 'rows': 15})
