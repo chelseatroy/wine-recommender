337ae7880db77565a56e6d957ae597351006d039
chelseatroy <chelsea.dommert@gmail.com>
2016-09-17 16:24:35 -0500

    Make wsgi.py files match

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "winerama.settings")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "winerama.settings")
+#
+# # This application object is used by the development server
+# # as well as any WSGI server configured to use this file.
+# import django.core.handlers.wsgi
+#
+# application = django.core.handlers.wsgi.WSGIHandler()
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = django.core.handlers.wsgi.WSGIHandler()
+application = Cling(get_wsgi_application())
