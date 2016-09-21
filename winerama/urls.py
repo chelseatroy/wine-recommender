d29269e65389e5967971f05e439bafb2d8dfe332
chelseatroy <chelsea.dommert@gmail.com>
2016-09-20 21:25:11 -0500

    send people somewhere from root url

--- winerama/urls.py
+++ winerama/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('reviews.urls', namespace="reviews")),
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
